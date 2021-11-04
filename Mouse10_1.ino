5c95852ec07baff90516fcf98824424c6f274981
DarthRobot <tyson@fabthis.net>
2021-11-04 14:24:24 -0400

    Add files via upload

---
+++ Mouse10_1.ino
@@ -0,0 +1,20 @@
+#include <Mouse.h>
+
+
+int responseDelay = 9000;        // response delay of the mouse, in ms
+
+
+
+void setup() {
+    Mouse.begin();
+}
+
+void loop() {
+
+
+  // move the mouse:
+  Mouse.move(1, 1, 0);
+  delay(responseDelay);
+   delay(responseDelay);
+}
+
