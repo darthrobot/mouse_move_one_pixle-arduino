016c5340634fd1bec5e583f6df8ea65b56582428
DarthRobot <tyson@fabthis.net>
2024-09-23 09:05:10 -0400

    different date on file

--- Mouse10_1.ino
+++ Mouse10_1.ino
@@ -1,20 +1,11 @@
 #include <Mouse.h>
-
-
 int responseDelay = 9000;        // response delay of the mouse, in ms
-
-
-
 void setup() {
     Mouse.begin();
 }
-
 void loop() {
-
-
   // move the mouse:
   Mouse.move(1, 1, 0);
   delay(responseDelay);
    delay(responseDelay);
 }
-
